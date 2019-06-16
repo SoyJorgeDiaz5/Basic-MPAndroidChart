c9518978ea265f0ad1c58bd36da415b84c72ca08
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2019-06-16 17:57:15 -0500

    PieChart implementation

--- app/src/main/java/com/co/soyjorgediaz5/basicmpandroidchart/MainActivity.java
+++ app/src/main/java/com/co/soyjorgediaz5/basicmpandroidchart/MainActivity.java
@@ -2,13 +2,61 @@ package com.co.soyjorgediaz5.basicmpandroidchart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pieChart = findViewById(R.id.pie_chart);
+        pieChart.setUsePercentValues(true);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+        pieChart.setDragDecelerationFrictionCoef(0.15f);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+        pieChart.animateY(1000, Easing.EaseInOutCubic);
+        pieChart.setEntryLabelColor(Color.BLACK);
+
+        List<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(25f, "Colombia", Color.BLUE));
+        yValues.add(new PieEntry(25f, "Argentina"));
+        yValues.add(new PieEntry(30f, "Chile"));
+        yValues.add(new PieEntry(10f, "Perú"));
+        yValues.add(new PieEntry(10f, "Venezuela"));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData pieData = new PieData(dataSet);
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.BLACK);
+        pieChart.setData(pieData);
+
+        Description description = new Description();
+        description.setText("Algunos participantes del torneo");
+        description.setTextSize(15);
+        pieChart.setDescription(description);
+
     }
 }
